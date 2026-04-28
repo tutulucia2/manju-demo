4b31251c4f1b2d93efbd48798967149ab25842da
tutulucia2 <tutulucia2@gmail.com>
2026-04-28 09:55:27 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,1 @@
-// Local default. Replace this value with your deployed Render/Railway API URL before publishing.
-window.MANJU_API_BASE = "http://127.0.0.1:8010";
+window.MANJU_API_BASE = "https://manju-agent-api.onrender.com";
